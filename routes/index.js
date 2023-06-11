045b032346fabe6d54876f64ab770ce58c62c08d
BuildTools <justin.d.bench@gmail.com>
2023-06-10 23:42:13 -0600

    setup local auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const users = require("./users.js");
 const posts = require("./posts.js");
 const swagger = require("./swagger.js");
+const { requiresAuth } = require("express-openid-connect");
 
 const routes = require("express").Router();
 
@@ -8,5 +9,8 @@ routes.use("/", swagger);
 routes.use("/api-docs", swagger);
 routes.use("/users", users);
 routes.use("/posts", posts);
+routes.use("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const app = express();
 const mongo = require("./db/mongo.js");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
+const { auth } = require("express-openid-connect");
 const port = process.env.PORT || 8080;
+
+const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+};
 
 app
   .use(bodyParser.json())
@@ -13,6 +23,7 @@ app
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
   })
+  .use(auth(authConfig))
   .use("/", routes);
 
 mongo.initDb((e) => {
